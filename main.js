ff38ef06db604c36f697ad6c6edbd689c3153b68
kosert <robert.kosakowski@student.put.poznan.pl>
2017-12-01 18:16:26 +0100

    works

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var ytdl = require('ytdl-core')
 var qs = require('querystring')
+var path = require('path')
 
 var express = require('express')
 var app = express()
@@ -7,10 +8,8 @@ var app = express()
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile("index.html")
+    res.sendFile(path.join(__dirname, "/public", "index.html"))
 })
-
-
 
 app.post('/validateURL', (req, res) => {
 
@@ -28,11 +27,41 @@ app.post('/validateURL', (req, res) => {
         var post = qs.parse(body);
         // use post['blah'], etc.
         var url = post['url']
+
         var result = ytdl.validateURL(url)
 
         res.end(result.toString())
     })
 })
+
+app.post('/getInfo', (req, res) => {
+    var body = '';
+    req.on('data', function (data) {
+        body += data;
+
+        // Too much POST data, kill the connection!
+        // 1e3 === 1 * Math.pow(10, 3) === 1 * 1000 ~~~ 1KB
+        if (body.length > 1e3)
+            req.connection.destroy();
+    })
+
+    req.on('end', function () {
+        var post = qs.parse(body);
+        // use post['blah'], etc.
+        var url = post['url']
+
+        ytdl.getInfo(url, function(err, info){
+            if(err)
+                console.log(err)
+            else
+            {
+                //for(f of info.formats) console.log(f.type, f.encoding, f.quality, f.container, f.resolution)
+                res.end(JSON.stringify(info.formats))
+            }
+        })
+
+    })
+});
 
 var server = app.listen(2137, () => {
     console.log(`Server started on ` + 2137);
--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,9 @@
 window.onload = function () {
     var inputURL = document.getElementById('input-url')
-    var icon = document.getElementById('icon')
     var button = document.getElementById('button-download')
-    var inputParent = inputURL.parentElement
     var lastUrl = ""
 
-    inputURL.addEventListener('keyup', function () {
+    function onInput() {   
         if (inputURL.value === "")
         {
             lastUrl = ""
@@ -25,38 +23,92 @@ window.onload = function () {
                     else {
                         failed()
                     }
-                    console.log(xmlhttp.responseText)
                 }
             }
             xmlhttp.send(param)
         }
-    })
+    }
 
+    function callOnInput() {
+        setTimeout(onInput, 0)
+    }
+
+    inputURL.addEventListener('keyup', callOnInput)
+    inputURL.addEventListener('paste', callOnInput)
+
+    button.addEventListener('click', function(){
+
+        var table = document.getElementById('formats')
+        var oldTbody = table.getElementsByTagName('tbody')[0]
+        var newTbody = document.createElement('tbody')
+
+        var loadingDiv = document.createElement('div')
+        loadingDiv.classList.add('progress')
+        var progressBar = document.createElement('div')
+        progressBar.classList.add('progress-bar', 'progress-bar-striped', 'progress-bar-animated', 'w-100')
+        loadingDiv.appendChild(progressBar)
+        var loadingRow = newTbody.insertRow(0)
+        var loadingCell = loadingRow.insertCell()
+        loadingCell.style = "height: 50px;"
+        loadingCell.classList.add('align-middle')
+        loadingCell.appendChild(loadingDiv)
+        loadingCell.colSpan = "5"
+
+        table.replaceChild(newTbody, oldTbody)
+        table.hidden = false
+
+        var param = "url=" + lastUrl
+        xmlhttp = new XMLHttpRequest()
+        xmlhttp.open("POST", "/getInfo", true)
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var filters = JSON.parse(xmlhttp.responseText)
+
+                newTbody.deleteRow(0)
+
+                for (f of filters) {
+                    var row = newTbody.insertRow(newTbody.rows.length)
+                    row.insertCell().innerHTML = f.encoding ? f.encoding : "<b>NO VIDEO</b>"
+                    row.insertCell().innerHTML = f.audioEncoding ? f.audioEncoding : "<b>NO AUDIO</b>"
+                    row.insertCell().innerHTML = f.container
+                    row.insertCell().innerHTML = f.resolution
+                    var link = document.createElement('a')
+                    link.href = f.url
+                    var linkText = document.createTextNode('Click here')
+                    link.appendChild(linkText)
+                    link.title = 'Click here'
+                    row.insertCell().appendChild(link)
+                }
+
+            }
+        }
+        xmlhttp.send(param)
+
+    })
 
     function startLoading() {
         inputURL.classList.add("loading")
-        inputParent.classList.remove("has-success")
-        inputParent.classList.remove("has-error")
-        icon.classList.remove("glyphicon-ok")
-        icon.classList.remove("glyphicon-remove")
-        button.classList.remove("active")
-        button.classList.add("disabled")
+        inputURL.classList.remove("is-valid")
+        inputURL.classList.remove("is-invalid")
+        button.classList.add('btn-secondary')
+        button.classList.remove('btn-primary')
+        button.disabled = true
     }
 
     function success() {
         inputURL.classList.remove("loading")
-        inputParent.classList.add("has-success")
-        icon.classList.add("glyphicon-ok")
-        button.classList.remove("disabled")
-        button.classList.add("active")
+        inputURL.classList.add("is-valid")
+        button.classList.add('btn-primary')
+        button.classList.remove('btn-secondary')
+        button.disabled = false
     }
 
     function failed() {
         inputURL.classList.remove("loading")
-        inputParent.classList.add("has-error")
-        icon.classList.add("glyphicon-remove")
-        button.classList.remove("active")
-        button.classList.add("disabled")
+        inputURL.classList.add("is-invalid")
+        button.classList.add('btn-secondary')
+        button.classList.remove('btn-primary')
+        button.disabled = true
     }
 
     console.log("Init completed.")
