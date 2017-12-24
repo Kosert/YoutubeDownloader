169092c631cc2c304ab772ff2aaf175ffbe0ae25
kosert <robert.kosakowski@student.put.poznan.pl>
2017-12-24 16:07:16 +0100

    major improvements

--- main.js
+++ main.js
@@ -7,14 +7,15 @@ var express = require('express')
 var app = express()
 
 app.use(morgan('dev'))
-
-app.use(express.static(path.join(__dirname, '/public')))
+//TODO REMOVE "/YOUTUBE" BEFORE COMMITING
+app.use('/youtube', express.static(path.join(__dirname, '/public')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/public", "index.html"))
 })
 
-app.post('//validateURL', (req, res) => {
+//TODO "//validateURL" BEFORE COMMITING
+app.post('/youtube/validateURL', (req, res) => {
 
     var body = '';
     req.on('data', function (data) {
@@ -36,20 +37,46 @@ app.post('//validateURL', (req, res) => {
     })
 })
 
-app.post('//getInfo', (req, res) => {
-    var body = '';
-    req.on('data', function (data) {
-        body += data;
+//TODO "//getInfo" BEFORE COMMITING
+app.post('/youtube/getInfo', (req, res) => {
 
-        // Too much POST data, kill the connection!
-        // 1e3 === 1 * Math.pow(10, 3) === 1 * 1000 ~~~ 1KB
-        if (body.length > 1e3)
-            req.connection.destroy();
-    })
-
-    req.on('end', function () {
-        var post = qs.parse(body);
+    getPostData(req, function(post) {
         var url = post['url']
+        ytdl.getInfo(url, function(err, info){
+            if(err)
+            {
+                var response = {
+                    error: err.message.replace(/&quot;/g, '\"')
+                }
+                res.end(JSON.stringify(response))
+            }
+            else
+            {
+                //for(f of info.formats) console.log(f.type, f.encoding, f.quality, f.container, f.resolution)
+                var response = {
+                    title: info.title,
+                    thumb: info.thumbnail_url,
+                    author: info.author.name,
+                    url: info.video_url,
+                    formats: info.formats
+                }
+
+                res.end(JSON.stringify(response))
+            }
+        })
+    })
+});
+
+//TODO "//getConvertUrl" BEFORE COMMITING
+app.post('/youtube/getConvertUrl', (req, res) => {
+   
+    getPostData(req, function(post) {
+        var url = post['url']
+        var audioItag = post['audio']
+        var videoItag = post['video']        
+
+        //TODO
+
 
         ytdl.getInfo(url, function(err, info){
             if(err)
@@ -65,10 +92,27 @@ app.post('//getInfo', (req, res) => {
                 res.end(JSON.stringify(info.formats))
             }
         })
-
     })
 });
 
 var server = app.listen(2137, () => {
     console.log(`Server started on ` + 2137);
 })
+
+function getPostData(req, callback) {
+
+    var body = '';
+    req.on('data', function (data) {
+        body += data;
+
+        // Too much POST data, kill the connection!
+        // 1e3 === 1 * Math.pow(10, 3) === 1 * 1000 ~~~ 1KB
+        if (body.length > 1e3)
+            req.connection.destroy();
+    })
+
+    req.on('end', function () {
+        var post = qs.parse(body);
+        callback(post)
+    })
+}
--- public/js/index.js
+++ public/js/index.js
@@ -4,8 +4,7 @@ window.onload = function () {
     var lastUrl = ""
 
     function onInput() {
-        if (inputURL.value === "")
-        {
+        if (inputURL.value === "") {
             lastUrl = ""
             failed()
         }
@@ -38,10 +37,180 @@ window.onload = function () {
 
     button.addEventListener('click', function () {
 
-        var table = document.getElementById('formats')
+        var divLoading = document.getElementById('div-loading')
+        divLoading.hidden = false
+
+        var divInfo = document.getElementById('div-info')
+        divInfo.hidden = true
+
+        var divCustom = document.getElementById('div-custom')
+        divCustom.hidden = true
+
+        var divTracks = document.getElementById('div-tracks')
+        divTracks.hidden = true
+
+        var divFulls = document.getElementById('div-fulls')
+        divFulls.hidden = true
+
+        var fullsTable = document.getElementById('fulls')
+        var videoTable = document.getElementById('videoOnly')
+        var audioTable = document.getElementById('audioOnly')
+        clearAndLoad(fullsTable)
+        clearAndLoad(videoTable)
+        clearAndLoad(audioTable)
+
+        var videoSelect = document.getElementById('video-select')
+        var audioSelect = document.getElementById('audio-select')
+
+        while (videoSelect.options.length > 0) {
+            videoSelect.remove(0)
+        }
+
+        while (audioSelect.options.length > 0) {
+            audioSelect.remove(0)
+        }
+
+        var fulls = []
+        var videoOnly = []
+        var audioOnly = []
+
+        var param = "url=" + lastUrl
+        xmlhttp = new XMLHttpRequest()
+        xmlhttp.open("POST", "/youtube/getInfo", true)
+        xmlhttp.onreadystatechange = function () {
+            divLoading.hidden = true
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var response = JSON.parse(xmlhttp.responseText)
+
+                if (response.error) {
+                    fullsTable.tBodies[0].deleteRow(0)
+                    videoTable.tBodies[0].deleteRow(0)
+                    audioTable.tBodies[0].deleteRow(0)
+
+                    var errorRow = fullsTable.tBodies[0].insertRow(0)
+                    var errorCell = errorRow.insertCell()
+                    errorCell.colSpan = "5"
+                    errorCell.classList.add("alert", "alert-danger")
+                    errorCell.innerText = "Error: " + response.error
+                }
+                else {
+                    console.log(response)
+                    for (f of response.formats) {
+                        if (f.encoding && f.audioEncoding) {
+                            fulls.push(f)
+                        }
+                        else if (f.encoding && !f.audioEncoding) {
+                            videoOnly.push(f)
+                            var option = document.createElement('option')
+                            option.setAttribute('data-itag', f.itag)
+                            option.text = formatToString(f)
+                            videoSelect.add(option)
+                        }
+                        else {
+                            var option = document.createElement('option')
+                            option.setAttribute('data-itag', f.itag)
+                            option.text = formatToString(f)
+                            audioSelect.add(option)
+                            audioOnly.push(f)
+                        }
+                    }
+
+                    document.getElementById('title').innerText = response.title
+                    document.getElementById('description').innerText = "Uploaded by: " + response.author
+                    document.getElementById('thumbnail').src = response.thumb
+                    divInfo.setAttribute('data-url', response.url)
+                    
+                    divInfo.hidden = false
+                    divFulls.hidden = false
+                    divTracks.hidden = false
+
+                    var convertOld = document.getElementById('convert')
+                    var convertButton = convertOld.cloneNode();
+                    convertOld.parentNode.replaceChild(convertButton, convertOld);
+                    convertButton.innerText = "Convert"
+
+                    convertButton.addEventListener('click', function () {
+                        convertButton.disabled = true
+                        var videoItag = videoSelect.options[videoSelect.selectedIndex].getAttribute('data-itag')
+                        var audioItag = audioSelect.options[audioSelect.selectedIndex].getAttribute('data-itag')
+                        convert(divInfo.getAttribute('data-url'), videoItag, audioItag)
+                    })
+
+                    divCustom.hidden = false
+
+                    populateTable(fullsTable, fulls)
+                    populateTable(videoTable, videoOnly)
+                    populateTable(audioTable, audioOnly)
+
+                    console.log("fulls:")
+                    console.log(fulls)
+                    console.log("video:")
+                    console.log(videoOnly)
+                    console.log("audio:")
+                    console.log(audioOnly)
+                }
+            }
+        }
+        xmlhttp.send(param)
+
+    })
+
+    function formatToString(f) {
+
+        var encoding = f.encoding ? f.encoding : f.audioEncoding
+        var container = f.container
+        var quality = f.encoding ? f.resolution + (f.fps ? " " + f.fps + "fps" : "") : f.audioBitrate + " kbps"
+        return encoding + " | " + container + " | " + quality
+    }
+
+    function convert(url, videoTag, audioTag) {
+        
+        var params = "url=" + url + "&video=" + videoTag + "&audio=" + audioTag
+        xmlhttp = new XMLHttpRequest()
+        xmlhttp.open("POST", "/youtube/getConvertUrl", true)
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var response = JSON.parse(xmlhttp.responseText)
+
+                if(response.error)
+                {
+                    //TODO
+                }
+                else
+                {
+                    window.location.href = response.url
+                }
+            }
+        }
+        xmlhttp.send(params)
+    }
+
+    function populateTable(table, data) {
+        for (f of data) {
+            var tbody = table.tBodies[0]
+            var row = tbody.insertRow(tbody.rows.length)
+            if (f.encoding) {
+                row.insertCell().innerHTML = f.encoding
+            }
+            if (f.audioEncoding) {
+                row.insertCell().innerHTML = f.audioEncoding
+            }
+            row.insertCell().innerHTML = f.container
+            row.insertCell().innerHTML = f.encoding ? f.resolution + (f.fps ? " " + f.fps + "fps" : "") : f.audioBitrate + " kbps"
+            var link = document.createElement('a')
+            link.href = f.url
+            link.download = document.getElementById("title").innerText
+            var linkText = document.createTextNode('Download')
+            link.appendChild(linkText)
+            link.title = 'Click here'
+            row.insertCell().appendChild(link)
+        }
+    }
+
+    function clearAndLoad(table) {
         var oldTbody = table.getElementsByTagName('tbody')[0]
         var newTbody = document.createElement('tbody')
-
+/*
         var loadingDiv = document.createElement('div')
         loadingDiv.classList.add('progress')
         var progressBar = document.createElement('div')
@@ -53,50 +222,9 @@ window.onload = function () {
         loadingCell.classList.add('align-middle')
         loadingCell.appendChild(loadingDiv)
         loadingCell.colSpan = "5"
-
+*/
         table.replaceChild(newTbody, oldTbody)
-        table.hidden = false
-
-        var param = "url=" + lastUrl
-        xmlhttp = new XMLHttpRequest()
-        xmlhttp.open("POST", "/youtube/getInfo", true)
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                var filters = JSON.parse(xmlhttp.responseText)
-
-                if (filters.error) {
-                    newTbody.deleteRow(0)
-                    var errorRow = newTbody.insertRow(0)
-                    var errorCell = errorRow.insertCell()
-                    errorCell.colSpan = "5"
-                    errorCell.classList.add("alert", "alert-danger")
-                    errorCell.innerText = "Error: " + filters.error
-                }
-                else {
-                    newTbody.deleteRow(0)
-
-                    for (f of filters) {
-                        var row = newTbody.insertRow(newTbody.rows.length)
-                        row.insertCell().innerHTML = f.encoding ? f.encoding : "<b>NO VIDEO</b>"
-                        row.insertCell().innerHTML = f.audioEncoding ? f.audioEncoding : "<b>NO AUDIO</b>"
-                        row.insertCell().innerHTML = f.container
-                        row.insertCell().innerHTML = f.encoding ? f.resolution + ( f.fps ? " " + f.fps + "fps" : "" ) : f.audioBitrate + " kbps"
-                        var link = document.createElement('a')
-                        link.href = f.url
-                        var linkText = document.createTextNode('Click here')
-                        link.appendChild(linkText)
-                        link.title = 'Click here'
-                        row.insertCell().appendChild(link)
-                    }
-                }
-
-
-
-            }
-        }
-        xmlhttp.send(param)
-
-    })
+    }
 
     function startLoading() {
         inputURL.classList.add("loading")
