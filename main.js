e6c3e4804dd7f2d5cf463e5ecb14625fbfa7b66c
kosert <robert.kosakowski@student.put.poznan.pl>
2017-12-30 14:38:26 +0100

    small imprevement

--- main.js
+++ main.js
@@ -23,22 +23,10 @@ app.get('/', (req, res) => {
 //TODO "//validateURL" BEFORE COMMITING
 app.post('/youtube/validateURL', (req, res) => {
 
-    var body = '';
-    req.on('data', function (data) {
-        body += data;
-
-        // Too much POST data, kill the connection!
-        // 1e3 === 1 * Math.pow(10, 3) === 1 * 1000 ~~~ 1KB
-        if (body.length > 1e3)
-            req.connection.destroy();
-    })
-
-    req.on('end', function () {
-        var post = qs.parse(body);
+    getPostData(req, function(post) {
         var url = post['url']
 
         var result = ytdl.validateURL(url)
-
         res.end(result.toString())
     })
 })
